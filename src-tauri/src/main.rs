9f41599672692f47b64b667ffc82facb48230ed5
Poyotoron <u729356b@ecs.osaka-u.ac.jp>
2024-04-08 13:11:13 +0900

    Version 0.1.0 (#8)
    * ボタン状態の可視化 (#1)
    
    * 初ビルド
    
    * 皿と鍵盤の画像表示
    
    * READMEにプログラムの説明を追加
    
    * ボタン状態をコントローラーから読んでViewに送信する部分を作成
    
    * ボタン状態を受け取って画面に反映できるようにした
    
    ---------
    
    Co-authored-by: Poyotoron <poyo@poyopoyo>
    
    * Feature/view scratch state (#2)
    
    * スクラッチの状態をViewに送信するようにした
    
    * Rustからスクラッチの状態を受け取り値に応じてViewの画像を回転
    
    ---------
    
    Co-authored-by: Poyotoron <poyo@poyopoyo>
    
    * Feature/update scratch img (#3)
    
    * png -> svg
    
    * スクラッチ画像関連の様々な修正
    
    * 画面をスクロールしないように
    
    ---------
    
    Co-authored-by: Poyotoron <poyo@poyopoyo>
    
    * Revert "Feature/update scratch img (#3)" (#4)
    
    This reverts commit d8eda8fefe6ba014c7bfd745b8686b4acbbe27fb.
    
    * Feature/update scratch img (#5)
    
    * png -> svg
    
    * スクラッチ画像関連の様々な修正
    
    * 画面をスクロールしないように
    
    * inkscape製のスクラッチに差し替え
    
    ---------
    
    Co-authored-by: Poyotoron <poyo@poyopoyo>
    
    * Feature/show-count (#6)
    
    * カウント表示機能を追加
    
    * スクラッチ・ボタンカウント送信機能の追加
    
    * 鍵盤別カウンタを消してカウンタの文字を大きくした
    
    * Feature/prepare-release-0.1.0 (#7)
    
    * バージョンを0.1.0に変更
    
    * Github Actionsでビルドするためのファイルを追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,173 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use std::thread;
+use std::time::Duration;
+use std::{process::exit, sync::mpsc};
+
+use tauri::Manager;
+use windows::Gaming::Input::{GameControllerSwitchPosition, RawGameController};
+
+// 接続中のコントローラーを取得して0番目を返す
+fn connect() -> Result<RawGameController, &'static str> {
+    let controller = RawGameController::RawGameControllers().unwrap();
+    if controller.Size().unwrap() > 0 {
+        Ok(controller.GetAt(0).unwrap())
+    } else {
+        Err("No controller found")
+    }
+}
+
+// 動き始めはコントローラーを認識しないのでループで待つ
+fn connect_wait() -> Result<RawGameController, &'static str> {
+    loop {
+        match connect() {
+            Ok(controller) => return Ok(controller),
+            Err(_) => std::thread::sleep(std::time::Duration::from_secs(1)),
+        }
+    }
+}
+
+struct Scratch {
+    // 1つ前のスクラッチの値
+    prev_value: f32,
+    // 1つ前のスクラッチのアクティブ状態
+    prev_active: bool,
+    // 1つ前のスクラッチの方向
+    prev_dir: bool,
+    // 停止検知カウンタ
+    counter: i32,
+}
+
+impl Scratch {
+    pub fn new() -> Self {
+        // TODO: counterは設定可能に
+        Self {
+            prev_value: 0.0,
+            prev_active: false,
+            prev_dir: false,
+            counter: 500,
+        }
+    }
+
+    pub fn check_input(&mut self, value: f32) -> bool {
+        let scratch_value: f32 = value;
+        let mut scratch_active = self.prev_active;
+        let mut scratch_dir = self.prev_dir;
+
+        let mut scratch_started = false;
+
+        if self.prev_value != scratch_value {
+            scratch_active = true;
+            self.counter = 0;
+
+            let scratch_diff = scratch_value - self.prev_value;
+            if scratch_diff.abs() > 0.8 {
+                if scratch_diff < 0.0 {
+                    scratch_dir = true;
+                } else {
+                    scratch_dir = false;
+                }
+            } else {
+                if scratch_diff > 0.0 {
+                    scratch_dir = true;
+                } else {
+                    scratch_dir = false;
+                }
+            }
+        } else {
+            self.counter += 1;
+
+            if self.counter > 50 {
+                scratch_active = false;
+            }
+        }
+
+        if (scratch_active && !self.prev_active) || (scratch_dir != self.prev_dir) {
+            scratch_started = true;
+        }
+
+        self.prev_value = scratch_value;
+        self.prev_active = scratch_active;
+        self.prev_dir = scratch_dir;
+
+        return scratch_started;
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            // Rust→Viewの通信用ハンドル
+            let app_handle = app.app_handle();
+
+            // コントローラー接続
+            let controller: RawGameController = connect_wait().unwrap();
+
+            // 100マイクロ秒ごとにコントローラーの状態を取る
+            // ボタンとスクラッチの状態と押下・回転開始をViewに送る
+            thread::spawn(move || {
+                let mut scratch = Scratch::new();
+
+                let prev_button_state = &mut [false; 16];
+                let button_state = &mut [false; 16];
+                let switch_state = &mut [GameControllerSwitchPosition::Center];
+                let axis_state = &mut [0.0 as f64; 2];
+
+                let mut button_counts = [0; 7];
+                let mut scratch_count = 0;
+
+                loop {
+                    let _buttons = RawGameController::GetCurrentReading(
+                        &controller,
+                        button_state,
+                        switch_state,
+                        axis_state,
+                    )
+                    .unwrap();
+
+                    // ボタンの状態をViewに送る
+                    app_handle
+                        .emit_all("buttonState", &button_state[0..7])
+                        .unwrap();
+
+                    // 押下されたボタンをViewに送る
+                    for i in 0..button_counts.len() {
+                        if button_state[i] && !prev_button_state[i] {
+                            button_counts[i] += 1;
+                        }
+                    }
+
+                    app_handle
+                        .emit_all("buttonCounter", &button_counts)
+                        .unwrap();
+
+                    // スクラッチの状態をViewに送る
+                    app_handle.emit_all("scratchState", &axis_state[0]).unwrap();
+
+                    // スクラッチが回転開始の場合Viewに送る
+                    if scratch.check_input(axis_state[0] as f32) {
+                        scratch_count += 1;
+                    }
+
+                    // E1 + E4でリセット
+                    if button_state[8] && button_state[11] {
+                        for i in 0..button_counts.len() {
+                            button_counts[i] = 0;
+                        }
+                        scratch_count = 0;
+                    }
+
+                    app_handle.emit_all("scratchCount", scratch_count).unwrap();
+
+                    prev_button_state.copy_from_slice(button_state);
+
+                    thread::sleep(Duration::from_micros(100));
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,178 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React, { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { listen } from "@tauri-apps/api/event";
+
+const keyImage = "./src/assets/key.png"
+const keyImagePressed = "./src/assets/key_light.png"
+const scratchImage = "./src/assets/scratch.svg"
+
+const scratchAreaLeft = 0
+const scratchAreaTop = 0
+const scratchAreaWidth = 400
+const scratchAreaHeight = 400
+const scratchImageWidth = 350
+const scratchImageHeight = 350
+
+const keyAreaLeft = 400
+const keyAreaTop = 0
+const keyAreaWidth = 600
+const keyAreaHeight = 400
+const keyImageWidth = 80
+const keyImageHeight = 150
+
+const statAreaLeft = 1000
+const statAreaTop = 0
+const statAreaWidth = 400
+const statAreaHeight = 400
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [keyStates, setKeyStates] = useState([false, false, false, false, false, false, false]);
+  const [keyCounts, setKeyCounts] = useState([0, 0, 0, 0, 0, 0, 0]);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  const [scratchState, setScratchState] = useState(0.0);
+  const [scratchCount, setScratchCount] = useState(0);
+
+  function createKeyStyle(keyNum: number) {
+    var index = keyNum / 2;
+    var isWhite = keyNum % 2 == 1;
+
+    return {
+      position: "absolute",
+      left: 20 + keyNum * keyImageWidth,
+      top: isWhite ? 50 : 225,
+      width: keyImageWidth,
+      height: keyImageHeight,
+    };
   }
 
+  class Scratch extends React.Component {
+    render() {
+      return (
+        <img src={scratchImage} style={{ position: "absolute", left: (scratchAreaWidth - scratchImageWidth) / 2, top: (scratchAreaHeight - scratchImageHeight) / 2, width: scratchImageWidth, height: scratchImageHeight, transform: `rotate(${-scratchState * 360}deg)` }} />
+      );
+    }
+  }
+
+  class ScratchArea extends React.Component {
+    render() {
+      return (
+        <div style={{ position: "absolute", left: scratchAreaLeft, top: scratchAreaTop }}>
+          <Scratch />
+        </div>
+      );
+    }
+  }
+
+  class Key extends React.Component<{ src: string, keyNum: number }> {
+    render() {
+      return (
+        <img src={this.props.src} style={createKeyStyle(this.props.keyNum) as React.CSSProperties} />
+      )
+    }
+  }
+
+  class Keys extends React.Component {
+    render() {
+      const keys = [];
+      for (var i = 0; i < 7; i++) {
+        if (keyStates[i]) {
+          keys.push(<Key src={keyImagePressed} keyNum={i} />);
+        } else {
+          keys.push(<Key src={keyImage} keyNum={i} />);
+        }
+      }
+
+      return (
+        <span>
+          {keys}
+        </span>
+      );
+    }
+  }
+
+  class KeyArea extends React.Component {
+    render() {
+      return (
+        <div style={{ position: "absolute", left: keyAreaLeft, top: keyAreaTop }}>
+          <Keys />
+        </div>
+      );
+    }
+  }
+
+  class StatArea extends React.Component {
+    render() {
+      const keyCountViews = [];
+      let keyCountSum = 0;
+      for (var i = 0; i < 7; i++) {
+        keyCountViews.push(<li>{keyCounts[i]}</li>);
+        keyCountSum += keyCounts[i];
+      }
+
+      return (
+        <div style={{ position: "absolute", left: statAreaLeft, top: statAreaTop, fontSize: 48 }}>
+          <p>Key Count:</p>
+          <p style={{ textAlign: "center" }}>{keyCountSum}</p>
+          <p>Scratch Count:</p>
+          <p style={{ textAlign: "center" }}>{scratchCount}</p>
+        </div>
+      );
+    }
+  }
+
+  useEffect(() => {
+    let listenButtonState: any;
+    let listenButtonCounter: any;
+    let listenScratchState: any;
+    let listenScratchCount: any;
+
+    async function f() {
+      listenButtonState = await listen("buttonState", event => {
+        console.log(event.payload);
+        setKeyStates(event.payload as boolean[]);
+      });
+
+      listenButtonCounter = await listen("buttonCounter", event => {
+        console.log(event.payload);
+        setKeyCounts(event.payload as number[]);
+      });
+
+      listenScratchState = await listen("scratchState", event => {
+        console.log(event.payload);
+        setScratchState(event.payload as number);
+      });
+
+      listenScratchCount = await listen("scratchCount", event => {
+        console.log(event.payload);
+        setScratchCount(event.payload as number);
+      });
+    }
+    f();
+
+    return () => {
+      if (listenButtonState) {
+        listenButtonState();
+      }
+
+      if (listenButtonCounter) {
+        listenButtonCounter();
+      }
+
+      if (listenScratchState) {
+        listenScratchState();
+      }
+
+      if (listenScratchCount) {
+        listenScratchCount();
+      }
+    }
+  }, []);
+
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    <>
+      <ScratchArea /><KeyArea /><StatArea />
+    </>
   );
 }
 
