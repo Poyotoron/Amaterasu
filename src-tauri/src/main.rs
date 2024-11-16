54bb9b821f83c10b291fce84dc027aca30af8586
Poyotoron <20264082+Poyotoron@users.noreply.github.com>
2024-11-17 00:07:23 +0900

    emitの仕様変更を自動で調整してくれなかったので手動でやった

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,9 +5,9 @@ mod controller;
 
 use std::thread;
 use std::time::Duration;
-use tauri::{AppHandle, Manager};
+use tauri::{AppHandle, Emitter, Manager};
 
-fn controller_loop(app_handle: &AppHandle) {
+fn controller_loop(app_handle: AppHandle) {
     let mut controller = controller::Controller::new();
     controller.connect_wait(0);
     controller.init();
@@ -32,10 +32,10 @@ fn controller_loop(app_handle: &AppHandle) {
 
     // 初期カウントの送信
     app_handle
-        .emit_all("buttonCounter", controller.get_button_count())
+        .emit("buttonCounter", controller.get_button_count())
         .unwrap();
     app_handle
-        .emit_all("scratchCount", controller.get_scratch_count())
+        .emit("scratchCount", controller.get_scratch_count())
         .unwrap();
 
     // メインループ
@@ -48,12 +48,12 @@ fn controller_loop(app_handle: &AppHandle) {
         // コントローラーの状態をViewに送信
         if controller.get_button_diff() {
             app_handle
-                .emit_all("buttonState", controller.get_button_state())
+                .emit("buttonState", controller.get_button_state())
                 .expect("failed to send controller button state");
         }
         if scratch_send_counter >= scratch_send_interval {
             app_handle
-                .emit_all("scratchState", controller.get_scratch_state())
+                .emit("scratchState", controller.get_scratch_state())
                 .expect("failed to send controller scratch state");
             scratch_send_counter = 0;
         }
@@ -64,12 +64,12 @@ fn controller_loop(app_handle: &AppHandle) {
 
             if controller.get_button_count_diff() {
                 app_handle
-                    .emit_all("buttonCounter", controller.get_button_count())
+                    .emit("buttonCounter", controller.get_button_count())
                     .unwrap();
             }
             if controller.get_scratch_count_diff() {
                 app_handle
-                    .emit_all("scratchCount", controller.get_scratch_count())
+                    .emit("scratchCount", controller.get_scratch_count())
                     .unwrap();
             }
         }
@@ -82,10 +82,10 @@ fn controller_loop(app_handle: &AppHandle) {
             if !is_reset_pressed {
                 controller.reset_count();
                 app_handle
-                    .emit_all("buttonCounter", controller.get_button_count())
+                    .emit("buttonCounter", controller.get_button_count())
                     .unwrap();
                 app_handle
-                    .emit_all("scratchCount", controller.get_scratch_count())
+                    .emit("scratchCount", controller.get_scratch_count())
                     .unwrap();
                 is_reset_pressed = true;
             }
@@ -101,7 +101,7 @@ fn controller_loop(app_handle: &AppHandle) {
             if !is_pause_toggled {
                 is_pause = !is_pause;
                 is_pause_toggled = true;
-                app_handle.emit_all("togglePause", is_pause).unwrap();
+                app_handle.emit("togglePause", is_pause).unwrap();
             }
         } else {
             is_pause_toggled = false;
@@ -116,7 +116,7 @@ fn controller_loop(app_handle: &AppHandle) {
             if !is_2p_toggled {
                 is_2p = !is_2p;
                 is_2p_toggled = true;
-                app_handle.emit_all("toggle2P", is_2p).unwrap();
+                app_handle.emit("toggle2P", is_2p).unwrap();
             }
         } else {
             is_2p_toggled = false;
@@ -130,7 +130,7 @@ fn controller_loop(app_handle: &AppHandle) {
             if !is_save_pressed {
                 controller.save_count();
                 is_save_pressed = true;
-                app_handle.emit_all("savedCount", true).unwrap();
+                app_handle.emit("savedCount", true).unwrap();
             }
         } else {
             is_save_pressed = false;
@@ -146,10 +146,10 @@ fn main() {
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             // Rust→Viewの通信用ハンドル
-            let app_handle = app.app_handle();
+            let app_handle: AppHandle = app.app_handle().clone();
 
             // コントローラー周りのループ
-            thread::spawn(move || controller_loop(&app_handle));
+            thread::spawn(move || controller_loop(app_handle));
 
             Ok(())
         })
